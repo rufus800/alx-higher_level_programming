2815391808e742246c98fb72c7e0b1cb99990fd9
rufus800 <reajoanna06@gmail.com>
2022-12-20 10:15:32 +0000

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module define an empty class Square
+"""
+
+class Square:
+    """
+    Is an empty class of type Square
+    Attributes:
+        None
+    """
+   pass
